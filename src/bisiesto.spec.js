67a6d8a6c2740817f982559f3083eb60337bfddc
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-21 12:28:08 -0400

    El 5 no se considera como año bisiesto

--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -4,4 +4,11 @@ describe("Es bisiesto", () => {
   it("El 4 debería considerase como año bisiesto", () => {
     expect(esBisiesto(4)).toEqual(true);
   });
+
+  it("El 5 no debería considerase como año bisiesto", () => {
+    expect(esBisiesto(4)).toEqual(true);
+  });
+
+
+
 });
